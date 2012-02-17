91cd9519994ea739246559f2f8b27bee3efc8576
Ondrej Sykora <ondrasej@centrum.cz>
2012-02-17 15:53:54 +0100

    - debug output in the test was removed

--- src/minesweeper_test.cc
+++ src/minesweeper_test.cc
@@ -15,8 +15,6 @@
 // You should have received a copy of the GNU General Public License along with
 // MineSeeker. If not, see <http://www.gnu.org/licenses/>.
 
-#include <iostream>
-
 #include "common.h"
 #include "glog/logging.h"
 #include "gtest/gtest.h"
