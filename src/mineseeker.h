816c0fa25dfb9c96ae2631eb250d113c13877d63
Ondrej Sykora <ondrasej@centrum.cz>
2012-02-17 19:21:41 +0100

    - documentation comments

--- src/mineseeker.h
+++ src/mineseeker.h
@@ -41,10 +41,13 @@ class MineSeekerField {
     UNCOVERED = 2,
   };
 
+  // The number of all possible configurations. This is equal to the number of
+  // combinations of mines that can be around a given field.
   static const int kNumPossibleConfigurations;
 
   MineSeekerField();
 
+  // Returns true if the configuration can be assigned to this field.
   bool IsPossibleConfiguration(int configuration) const {
     DCHECK_GE(configuration, 0);
     DCHECK_LT(configuration, kNumPossibleConfigurations);
@@ -53,24 +56,37 @@ class MineSeekerField {
   // Returns true if this field may contain a mine, i.e. it was not uncovered
   // yet, or it was already proven to contain a mine.
   bool IsPossibleMine() const { return state_ != MINE; }
-
+  // Returns true if this field is bound, i.e. a single configuration is
+  // assigned to it.
   bool IsBound() const;
 
+  // Returns the number of configuration that can be assigned to this field
+  // (given its neighborhood).
   int NumberOfActiveConfigurations() const;
+  // Disables the specified configuration.
   void RemoveConfiguration(int configuration);
+  // Binds the field to a given configuration.
   void SetConfiguration(int configuration);
 
+  // Returns the state of the field.
   State state() const { return state_; }
+  // Changes the state of the field (but only updates the state, calling this
+  // method does not run propagation to other fields).
   void set_state(State state) { state_ = state; }
+  // Returns a bitmap with the possible configurations. For each configuration
+  // ID, this bitmap contains true if the configuration can be assigned to this
+  // field and false otherwise.
   const vector<bool>& configurations() const { return configurations_; }
 
  private:
+  // Resets the configurations - enables all configurations.
   void ResetConfigurations();
 
   State state_;
   vector<bool> configurations_;
 };
 
+// Contains coordinates of a field.
 struct FieldCoordinate {
   int x;
   int y;
@@ -79,6 +95,8 @@ struct FieldCoordinate {
       : x(x_coord), y(y_coord) {}
 };
 
+// Implements the mine seeking algorithm. Uses propagation and tree search to
+// prove the fields contain mines or not.
 class MineSeeker {
  public:
   MineSeeker(const MineSweeper& mine_sweeper);
@@ -89,17 +107,29 @@ class MineSeeker {
 
   const MineSeekerField& FieldAtPosition(int x, int y) const;
   MineSeekerField::State StateAtPosition(int x, int y) const;
-  void Solve();
 
+  // Runs the solver. Returns true if the game was successfully solved;
+  // otherwise, returns false.
+  bool Solve();
+
+  // Marks the given field as a field with mine. Runs propagation on its
+  // neighbors.
   void MarkAsMine(int x, int y);
+  // Uncovers the given field. If the field did not contain a mine, returns true
+  // and runs propagation on its neighbors. Otherwise, returns false and sets
+  // is_dead to true.
   bool UncoverField(int x, int y);
 
+  // Returns true if the mine seeker stepped on a mine.
   bool is_dead() const { return is_dead_; }
+  // Returns the MineSweeper instance on which the game is played.
   const MineSweeper& mine_sweeper() const { return mine_sweeper_; }
 
  private:
   typedef vector<vector<MineSeekerField> > MineSeekerState;
 
+  // Tests if the given configuration can be assigned to the field at position
+  // (x, y) with respect to its neighbor with relative coordinates (cx, cy).
   bool ConfigurationFitsWithSingleField(int configuration,
                                         int x,
                                         int x,
