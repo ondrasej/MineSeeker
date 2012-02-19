6daa252854577cf7d3b9c0abeeec6e8ebf4407bd
Ondrej Sykora <ondrasej@centrum.cz>
2012-02-19 19:23:45 +0100

    - project name in gtest_main was fixed

--- src/gtest/gtest_main.cc
+++ src/gtest/gtest_main.cc
@@ -35,7 +35,7 @@
 GTEST_API_ int main(int argc, char **argv) {
   std::cout << "Running main() from gtest_main.cc\n";
 
-  google::InitGoogleLogging("Ghoul");
+  google::InitGoogleLogging("MineSeeker");
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
